3f870e41bd1001ef452ef9e3010f95d45d4f6d68
tip <103219982+tip52@users.noreply.github.com>
2022-12-13 23:22:27 -0700

    Update RB battle wacky wizard.lua

--- RB battle wacky wizard.lua
+++ RB battle wacky wizard.lua
@@ -6,11 +6,13 @@ CoreGui:SetCore("SendNotification", {
 })
 
 task.spawn(function()
-while task.wait() do
-for i,v in pairs(workspace.ObbyFolder:GetDescendants()) do
-    if v.Name == "EndZone" and v:FindFirstChild("TouchInterest") then
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+    while task.wait() do
+        for i,v in pairs(workspace.ObbyFolder:GetDescendants()) do
+            if v.Name == "EndZone" and v:FindFirstChild("TouchInterest") then
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 0)
+                task.wait(.5)
+                firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, v, 1)
+            end
+        end
     end
-end
-end
 end)
